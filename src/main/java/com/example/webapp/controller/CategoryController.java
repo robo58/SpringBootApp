31311c6c14494948d37ce07d5eef4dc1685df3d6
robo58 <robertsliskovic@gmail.com>
2020-11-01 18:45:12 +0100

    Search,category filter

--- src/main/java/com/example/webapp/controller/CategoryController.java
+++ src/main/java/com/example/webapp/controller/CategoryController.java
@@ -1,15 +1,20 @@
 package com.example.webapp.controller;
 
+import java.util.List;
+
 import com.example.webapp.model.Category;
 import com.example.webapp.service.CategoryService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class CategoryController {
@@ -61,5 +66,5 @@ public class CategoryController {
         
         return "redirect:/categories";
     }
-    
+
 }
--- src/main/java/com/example/webapp/web/MainController.java
+++ src/main/java/com/example/webapp/web/MainController.java
@@ -1,20 +1,28 @@
 package com.example.webapp.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.example.webapp.service.BookService;
+import com.example.webapp.service.CategoryService;
 import com.example.webapp.web.dto.BookDto;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 
 @Controller
 public class MainController {
 
     @Autowired
     private BookService bookService;
+
+    @Autowired
+    private CategoryService categoryService;
 
     @GetMapping("/")
     public String showHome(Model model){
@@ -24,8 +32,20 @@ public class MainController {
 
     @GetMapping("/kategorija/{id}")
     public String kategorija(@PathVariable(value="id") long id, Model model){
-        model.addAttribute("books", this.bookService.getAllBooks());
-        return "index" + id;
+        model.addAttribute("books", this.categoryService.getCategorybyId(id).getBooks());
+        return "index";
+    }
+
+    @GetMapping("/search")
+    public String search(@RequestParam String query, Model model){
+        List<BookDto> books = new ArrayList<>();
+        this.bookService.getAllBooks().forEach(book -> {
+            if(book.getTitle().contains(query)){
+                books.add(book);
+            }
+        });
+        model.addAttribute("books", books);
+        return "index";
     }
 
     @GetMapping("/login")
